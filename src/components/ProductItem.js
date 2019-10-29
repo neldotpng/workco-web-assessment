e53680047aecb9d495566a25be9e4118c36f8ab7
Nelson Chang <nelsonschang@gmail.com>
2019-10-29 00:43:02 -0400

    add mobile and tablet styling for product list view

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,14 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Product from './Product'
 
-import image from '../assets/images/chronograph.png'
+import di from '../assets/images/chronograph.png'
+import ti from '../assets/images/chronograph-tablet.png'
+import mi from '../assets/images/chronograph-mobile.png'
+
 
 const ProductItem = ({ product, onAddToCartClicked }) => (
   <div className="productItem">
     <img
       className="productItem__image"
-      src={image}
-      alt=""/>
+      srcSet={`${mi} 480w, ${ti} 1024w, ${di} 1280w`}
+      src={di}
+      alt="Chronograph watch"/>
     <div className="productItem__info">
       <Product
         title={product.title}
